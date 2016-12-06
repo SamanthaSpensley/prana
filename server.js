d98479c82cef0dfc461bc49f7d9ecbf105df4cd4
Samantha Spensley <spenslsm@gmail.com>
2016-12-06 07:50:31 -0700

    add node-sass and express-sass modules

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+const compileSass = require('express-compile-sass')
+// const root = process.cwd();
+
+app.use(compileSass({
+  __dirname: __dirname,
+  sourceMap: true,
+  sourceComments: true,
+  watchFiles: true,
+  logToConsole: false
+}));
 
 app.use(express.static(__dirname));
 
